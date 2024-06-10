e8d0ec03f3d5dd6a144c84ced59eecc0859a644e
nico-campa123 <48675751@est.ort.edu.ar>
2024-06-10 14:25:18 -0300

    Initial commit

---
+++ Assets/interaction_area.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interaction_area : MonoBehaviour
+{
+    public GameObject filete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == filete) Debug.Log("CHOQUE FILETE");
+        Debug.Log("Colision");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Colision con " + other.name);
+    }
+}
---
+++ Assets/pickupscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickupscript : MonoBehaviour
+{
+    
+    public bool esAlimento;
+    public int valor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
